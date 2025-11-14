05284d8fc77d09ab1e36e76c2db97a0a5c3f1e8d
mahelika <imahelika@gmail.com>
2025-11-15 01:31:08 +0530

    add hello world in anchor

---
+++ counter/target/debug/deps/counter-12c1aae145ff7ee2.d
@@ -0,0 +1,5 @@
+/Users/mahelikaa/Desktop/RNG_Game/counter/target/debug/deps/counter-12c1aae145ff7ee2.d: src/main.rs
+
+/Users/mahelikaa/Desktop/RNG_Game/counter/target/debug/deps/libcounter-12c1aae145ff7ee2.rmeta: src/main.rs
+
+src/main.rs:
---
+++ counter/target/debug/deps/counter-f35674f19cc60dec.d
@@ -0,0 +1,5 @@
+/Users/mahelikaa/Desktop/RNG_Game/counter/target/debug/deps/counter-f35674f19cc60dec.d: src/main.rs
+
+/Users/mahelikaa/Desktop/RNG_Game/counter/target/debug/deps/libcounter-f35674f19cc60dec.rmeta: src/main.rs
+
+src/main.rs:
